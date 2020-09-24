e1e88081c7c3b0097a54f8144559c9ab495f1362
rf y <709241197@qq.com>
2020-09-25 00:41:17 +0800

    回文链表

--- post/test.js
+++ post/test.js
@@ -1,14 +1,36 @@
-const json = {
-  a: { b: { c: { d: 1 } } },
-  e: { f: {} },
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  if (!head || !head.next) {
+    return true;
+  }
+
+  let mid = head;
+  let pre = head;
+  let reversed = null;
+  while (head && head.next) {
+    mid = mid.next;
+    head = head.next.next;
+    // reversed
+    pre.next = reversed;
+    reversed = pre;
+    pre = mid;
+  }
+  // 奇数情况 mid 往后走一位
+  if (head) mid = mid.next;
+  while (mid) {
+    if (reversed.val !== mid.val) return false;
+    reversed = reversed.next;
+    mid = mid.next;
+  }
+  return true;
 };
-
-const path = ["a", "b"];
-
-let p = json;
-
-path.forEach((k) => {
-  p = p[k];
-});
-
-console.log(p);
