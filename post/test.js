bd072b1a6b8df468f4587e6df7bcc4e6d63b9bf1
rf y <709241197@qq.com>
2020-09-28 23:36:34 +0800

    rebuilding site 2020年 9月28日 星期一 23时36分34秒 CST

--- post/test.js
+++ post/test.js
@@ -1,15 +1,19 @@
-const arr = [5, 4, 3, 2, 1];
-
-Array.prototype.bubbleSort = function () {
+Array.prototype.selectionSort = function () {
   for (let i = 0; i < this.length - 1; i++) {
-    for (let j = 0; j < this.length - 1 - i; j++) {
-      if (this[j] > this[j + 1]) {
-        const temp = this[j];
-        this[j] = this[j + 1];
-        this[j + 1] = temp;
+    let min = i;
+    for (let j = i + 1; j < this.length; j++) {
+      if (this[j] < this[min]) {
+        min = j;
       }
+    }
+    if (min !== i) {
+      const temp = this[i];
+      this[i] = this[min];
+      this[min] = temp;
     }
   }
 };
 
-arr.bubbleSort();
+const arr = [5, 4, 3, 2, 1];
+arr.selectionSort();
+console.log(arr);
