0a5a400e5b53845f02ec77e0299df54acd9670b9
rf y <709241197@qq.com>
2020-11-10 02:09:18 +0800

    rebuilding site 2020年11月10日 星期二 02时09分18秒 CST

--- post/test.js
+++ post/test.js
@@ -1,22 +1,17 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @param {string} s
+ * @return {number}
  */
-var twoSum = function (nums, target) {
+var lengthOfLongestSubstring = function (s) {
+  let l = 0
+  let res = 0
   const map = new Map()
-  for (let i = 0; i < nums.length; i++) {
-    const item = nums[i]
-    const diff = target - item
-    if (map.has(diff)) {
-      return [map.get(diff), i]
-    } else {
-      map.set(item, i)
+  for (let r = 0; r < s.length; r++) {
+    if (map.has(s[r]) && map.get(s[r]) >= l) {
+      l = map.get(s[r]) + 1
     }
+    res = Math.max(res, r - l + 1)
+    map.set(s[r], r)
   }
+  return res
 }
-
-const nums = [2, 7, 11, 15],
-  target = 9
-
-console.log(twoSum(nums, target))
