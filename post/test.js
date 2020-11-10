786bcde45c011cf678eb1d0c42ea5c5b69c0400d
rf y <709241197@qq.com>
2020-11-11 00:30:22 +0800

    rebuilding site 2020年11月11日 星期三 00时30分22秒 CST

--- post/test.js
+++ post/test.js
@@ -1,43 +1,48 @@
-/**
- * @param {string} s
- * @param {string} t
- * @return {string}
- * 输入：s = "ADOBECODEBANC", t = "ABC"
- * 输出："BANC"
- */
-var minWindow = function (s, t) {
-  let l = 0
-  let r = 0
-  const map = new Map()
-  for (const c of t) {
-    map.set(c, map.has(c) ? map.get(c) + 1 : 1)
-  }
-  let mapSize = map.size
-  let res = ''
-  while (r < s.length) {
-    const right = s[r]
-    if (map.has(right)) {
-      map.set(right, map.get(right) - 1)
-      if (map.get(right) === 0) {
-        mapSize--
-      }
-    }
-    while (mapSize === 0) {
-      const newRes = s.substring(l, r + 1)
-      if (!res || res.length > newRes.length) {
-        res = newRes
-      }
-      const left = s[l]
-      if (map.has(left)) {
-        console.log(map)
-        map.set(left, map.get(left) + 1)
-        if (map.get(left) === 1) mapSize++
-      }
-      l++
-    }
-    r++
-  }
-  return res
+const tree = {
+  val: 'a',
+  children: [
+    {
+      val: 'b',
+      children: [
+        { val: 'd', children: [] },
+        { val: 'e', children: [] },
+      ],
+    },
+    {
+      val: 'c',
+      children: [
+        { val: 'f', children: [] },
+        { val: 'g', children: [] },
+      ],
+    },
+  ],
 }
 
-minWindow('ADOBECODEBANC', 'ABC')
+/**
+ * 深度优先遍历
+ * @param {*} root
+ */
+const dfs = (root) => {
+  console.log(root.val)
+  root.children.forEach(dfs)
+}
+
+/**
+ * 广度优先遍历
+ * @param {*} root
+ */
+const bfs = (root) => {
+  const q = [root]
+
+  while (q.length > 0) {
+    const n = q.shift()
+    console.log(n.val)
+    n.children.forEach((child) => {
+      q.push(child)
+    })
+  }
+}
+
+// dfs(tree)
+
+bfs(tree)
